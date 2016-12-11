d2437bcc326a7071da6dd8b8d9b3aa0f4c3596ef
Ethan Brown <e@zepln.com>
2016-12-10 21:52:46 -0500

    Added good set of tests, toString and toHtml functionality.

--- src/jsdifflist.js
+++ src/jsdifflist.js
@@ -3,6 +3,7 @@
 const chalk = require('chalk');
 const Table = require('easy-table');
 const isEqual = require('is-equal');
+const escapeHtml = require('escape-html');
 
 function equalityType(values, opts = {}) {
     if(values.length !== 2) throw new Error('Currently, only comparison between two objects is supported.');
@@ -28,58 +29,76 @@ function objdiff(objs, opts = {}) {
 function arraydiff(arrs, opts = {}) {
     if(arrs.length !== 2) throw new Error('Currently, only comparison between two arrays is supported.');
     // possibly a more efficient way to do this?
-    const allIndexes = new Array(Math.max(arrs.map(arr => arr.length))).fill(null).map((x, i) => i);
+    const allIndexes = new Array(Math.max(...arrs.map(arr => arr.length))).fill(null).map((x, i) => i);
     return allIndexes.map(index => Object.assign({ index }, equalityType(arrs.map(arr => arr[index]), opts)));
 }
 
-module.exports = function(a, b, opts = {}) {
-    const comparands = [a, b];
+function diff(comparands, opts) {
     if(comparands.every(c => c === null)) return equalityType(comparands, opts);
     if(comparands.every(c => typeof c === 'object')) {
-        if(!comparands.every(c => Array.isArray(c))) return equalityType(comparands, opts);
-        return comparands.every(c => Array.isArray(c)) ? arrdiff(comparands, opts) : objdiff(comparands, opts);
+        if(comparands.every(c => Array.isArray(c))) return arraydiff(comparands, opts);
+        if(comparands.some(c => Array.isArray(c))) return equalityType(comparands, opts);
+        return objdiff(comparands, opts);
     }
     return equalityType(comparands, opts);
 }
 
-/*
 module.exports = function(a, b, opts = {}) {
     if(typeof opts.numberFormat !== 'function') opts.numberFormat = n => n;
 
-    const allKeys = [...new Set([...Object.keys(a), ...Object.keys(b)])];
-
-    const t = new Table();
-
-    function formatValue(v, color) {
-        if(v === null) return chalk[color]('null'); // avoid JavaScript's brain-dead 'typeof' behavior wrt null
+    function formatValue(v) {
+        if(v === null) return 'null'; // avoid JavaScript's brain-dead 'typeof' behavior wrt null
         switch(typeof v) {
-            case 'string': return chalk[color]('"' + v.replace(/"/g, '\/"') + '"');
-            case 'number': return chalk[color](opts.numberFormat(v));
-            case 'object': return chalk[color](Array.isArray(v) ? 'Array' : 'Object');
+            case 'string': return '"' + v.replace(/"/g, '\/"') + '"';
+            case 'number': return opts.numberFormat(v);
+            case 'object': return Array.isArray(v) ? 'Array' : 'Object';
             // all other types have reasonable string representations
-            default: return chalk[color](v);
+            default: return v;
         }
     }
 
-    for(const key of allKeys) {
-        const color = a[key]===b[key] ? 'green' :
-            isEqual(a[key], b[key]) ? 'yellow' : 'red';
-        t.cell('Property', chalk[color](key));
-        t.cell('Object A', formatValue(a[key], color));
-        t.cell('Object B', formatValue(b[key], color));
-        t.newRow();
-    }
+    const results = diff([a, b], opts);
 
-    console.log(chalk.green('Green: Strictly Equal (===)'));
-    console.log(chalk.yellow('Yellow: Conceptually Equal (courtesy is-equal)'));
-    console.log(chalk.red('Red: Not Equal'));
-    console.log('\n' + t.toString());
+    Object.defineProperties(results, {
+        toString: {
+            isEnumerable: false,
+            value() {
+                const t = new Table();
+
+                for(const diff of this) {
+                    t.cell('Property', chalk[diff.colorHint](diff.key));
+                    t.cell('Equality', diff.result);
+                    t.cell('Object A', chalk[diff.colorHint](formatValue(diff.values[0])));
+                    t.cell('Object B', chalk[diff.colorHint](formatValue(diff.values[1])));
+                    t.newRow();
+                }
+
+                return chalk.green('Green: Strictly Equal (===)') + '\n' +
+                    chalk.yellow('Yellow: Conceptually Equal (courtesy is-equal)') + '\n' +
+                    chalk.red('Red: Not Equal') + '\n\n' +
+                    t.toString();
+            },
+        },
+        toHtml : {
+            isEnumerable: false,
+            value() {
+                return '<table class="jsdifflist">' +
+                    '<thead><tr>' +
+                    '<th>Property</th>' +
+                    '<th>Equality</th>' +
+                    '<th>Object A</th>' +
+                    '<th>Object B</th>' +
+                    '</tr></thead>' +
+                    '<tbody>' +
+                    this.map(d => `<tr class="jsdifflist-color-${d.colorHint}">` +
+                        `<td>${escapeHtml(d.key)}</td>` +
+                        `<td>${d.result}</td>` +
+                        `<td>${escapeHtml(formatValue(d.values[0]))}</td>` +
+                        `<td>${escapeHtml(formatValue(d.values[1]))}</td></tr>`).join('') +
+                    '</tbody></table>';
+            },
+        },
+    });
+
+    return results;
 }
-*/
-
-/*
-const a = { hey: 'a', nonny: 'b', no: 'c', x: '3', a: [1,2,3] };
-const b = { hey: 'a', nonny: 1, is: 2, fer: 3, horses: 4, x: 3, a: [1,2,3] };
-
-console.log(module.exports(a, b));
-*/
--- tests/jsdifflist-tests.js
+++ tests/jsdifflist-tests.js
@@ -42,3 +42,180 @@ tape('array vs primative', t => {
     t.plan(1);
     t.equal(diff([], 1).result, 'unequal');
 });
+
+tape('objects (strict)', t => {
+    const innerObj = {};
+    const innerArr = [];
+    const objA = {
+        a: null,
+        b: undefined,
+        c: true,
+        d: 1,
+        e: 's',
+        f: {},
+        g: [],
+    };
+    const diffs = diff(objA, Object.assign({}, objA));
+    t.plan(diffs.length + 1);
+    t.equal(diffs.length, Object.keys(objA).length, 'key count mismatch');
+    for(const d of diffs)
+       t.equal(d.result, 'strict'); 
+});
+
+tape('objects (loose)', t => {
+    const innerObj = {};
+    const innerArr = [];
+    const objA = {
+        a: null,
+        b: 1,
+        c: false,
+    };
+    const objB = {
+        a: undefined,
+        b: "1",
+        c: 0,
+    };
+    const diffs = diff(objA, objB);
+    t.plan(diffs.length);
+    for(const d of diffs)
+       t.equal(d.result, 'loose'); 
+});
+
+tape('objects (conceptual)', t => {
+    const objA = {
+        a: { x: 1, y: 2, z: 3 },
+        b: [ 10, 11, 12 ],
+    };
+    const objB = {
+        a: { x: 1, y: 2, z: 3 },
+        b: [ 10, 11, 12 ],
+    };
+    const diffs = diff(objA, objB);
+    t.plan(diffs.length);
+    for(const d of diffs)
+       t.equal(d.result, 'conceptual'); 
+});
+
+tape('objects (unequal)', t => {
+    const innerObj = {};
+    const innerArr = [];
+    const objA = {
+        a: 1,
+        b: true,
+        c: 'a',
+        d: [],
+        e: {},
+    };
+    const objB = {
+        a: 2,
+        b: false,
+        c: 'x',
+        d: [1],
+        e: {a: 1},
+    };
+    const diffs = diff(objA, objB);
+    t.plan(diffs.length + 1);
+    t.equal(diffs.length, Object.keys(objA).length, 'key count mismatch');
+    for(const d of diffs)
+       t.equal(d.result, 'unequal'); 
+});
+
+tape('arrays (strict)', t => {
+    const innerObj = {};
+    const innerArr = [];
+    const arrA = [
+        null,
+        undefined,
+        true,
+        1,
+        's',
+        {},
+        [],
+    ];
+    const diffs = diff(arrA, [...arrA]);
+    t.plan(diffs.length + 1);
+    t.equal(diffs.length, arrA.length, 'key count mismatch');
+    for(const d of diffs)
+       t.equal(d.result, 'strict'); 
+});
+
+tape('arrays (loose)', t => {
+    const innerObj = {};
+    const innerArr = [];
+    const arrA = [
+        null,
+        1,
+        false,
+    ];
+    const arrB = [
+        undefined,
+        "1",
+        0,
+    ];
+    const diffs = diff(arrA, arrB);
+    t.plan(diffs.length);
+    for(const d of diffs)
+       t.equal(d.result, 'loose'); 
+});
+
+tape('arrays (conceptual)', t => {
+    const arrA = [
+        { x: 1, y: 2, z: 3 },
+        [ 10, 11, 12 ],
+    ];
+    const arrB = [
+        { x: 1, y: 2, z: 3 },
+        [ 10, 11, 12 ],
+    ];
+    const diffs = diff(arrA, arrB);
+    t.plan(diffs.length);
+    for(const d of diffs)
+       t.equal(d.result, 'conceptual'); 
+});
+
+tape('arrays (unequal)', t => {
+    const innerObj = {};
+    const innerArr = [];
+    const arrA = [
+        1,
+        true,
+        'a',
+        {},
+        [],
+    ];
+    const arrB = [
+        2,
+        false,
+        'x',
+        {a: 1},
+        [1],
+    ];
+    const diffs = diff(arrA, arrB);
+    t.plan(diffs.length + 1);
+    t.equal(diffs.length, arrA.length, 'key count mismatch');
+    for(const d of diffs)
+       t.equal(d.result, 'unequal'); 
+});
+
+tape('objects with disjoint keys'), t => {
+    const objA = { a: 1, b: 2, c: 3 };
+    const objB = { x: 1, y: 2, z: 3 };
+    const diffs = diff(objA, objB);
+    t.plan(diffs.length + 1);
+    t.equal(diffs.length, 6, 'incorrect aggregate property count');
+    for(const d of diffs)
+        t.equal(d.result, 'unequal');
+}
+
+tape('objects with shared and unshared keys'), t => {
+    const objA = { a: 1, b: 2, c: 3 };
+    const objB = { x: 1, b: 2, z: 3 };
+    const diffs = diff(objA, objB);
+    t.plan(diffs.length + 1);
+    t.equal(diffs.length, 5, 'incorrect aggregate property count');
+    t.equal(diffs.find(x => x.key === 'a').result, 'strict');
+    t.equal(diffs.find(x => x.key === 'b').result, 'unequal');
+    t.equal(diffs.find(x => x.key === 'c').result, 'unequal');
+    t.equal(diffs.find(x => x.key === 'x').result, 'unequal');
+    t.equal(diffs.find(x => x.key === 'z').result, 'unequal');
+}
